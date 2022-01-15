c6396f8b85171a3a45fc5c17e3f4d5ec990339c6
Albert Ndizeye <albertndizeye@gmail.com>
2022-01-15 15:55:44 +0100

    suite

--- AJAX_TRAIN/Liste_Deroulante /script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#Chercher").click(function(){
-        var client = 'id='+ $('#clientId').val()
-        $("#resultat").load("data.php",client);
-    });  
-});
