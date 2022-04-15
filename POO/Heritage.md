02c6ebda117c64b298b45086e0da9cea5bb8a51a
Albert Ndizeye <albertndizeye@gmail.com>
2022-04-15 12:23:52 +0200

    suite

--- POO/Heritage.md
+++ POO/Heritage.md
@@ -334,5 +334,95 @@
 
         ```
 6. #### L'abstraction.
-    - Supposons que nous avons des **Utilisateaur** qui sont soit **Administrateur** soit **Joueur**. et que l'idée serait de ne pas instancier la classe ***Utilisateur*** seule et par conséquent de forcer son héritage.
+    - Supposons que nous avons des **Utilisateaur** qui sont soit **Administrateur** soit **Joueur**. et que l'idée serait de ne pas instancier la classe ***Utilisateur*** seule et par conséquent **d'imposer son héritage**.
+        -   ```
+                <?php
+                    declare(strict_types=1);
+                    abstract class Utilisateur
+                    {
+                        protected const STATUS_ACTIVE='ACTIVE';
+                        protected const STATUS_INACTIVE='INACTIVE';
+
+                        protected string $Username;
+                        protected string $Status; 
+
+                        Public static $nombreUtilisateur = 0;
+
+                        public function __construct(string $username, string $status=self::STATUS_ACTIVE){
+                            $this->Username=$username;
+                            $this->Status=$status;
+                        }
+                         public function setUsername(string $username):void{
+                             $this->Username = $username;
+                         }
+                         public function getUsername():string{
+                             return $this->Username;
+                         }
+
+                         public function setStatus(string $status):void{
+                             $this->Status=$status;
+                         }
+                         public function getStatus():string{
+                             return $this->Status;
+                         }
+                    }
+                         class Administrateur extends Utilisateur
+                            {
+                                private array $Role=[];
+
+                                public static $nombreAdm=0;
+
+                                public function __construct(string $username, string $status=self::STATUS_ACTIVE, array $role=[]){
+                                    parent::__construct($username,$status);
+                                    $this->Role=$role;
+                                    parent::$nombreUtilisateur++;
+                                    self::$nombreAdm++;
+                                }
+                                public function ajoutRole(string $role){
+                                    $this->Role[]=$role;
+                                    $this->Role = array_filter($this->Role);   
+                                }
+
+                                public function setRole(string $role):void{
+                                    $this->Role=$role;
+                                }
+
+                                public function getRole():array{
+                                    $role=$this->Role;
+                                    $role[]='ADMINISTRATEUR';
+
+                                    return $role;
+                                }
+                            }
+                             class Joueur extends Utilisateur
+                            {
+                                protected const Role='JOUEUR';
+                                protected string $Role;
+
+                                public static $nombreJe = 0;
+
+                                public function __construct(string $username, string $status=self::STATUS_ACTIVE, string $role=self::Role){
+                                    parent::__construct($username,$status);
+                                    $this->Role=$role;
+                                    parent::$nombreUtilisateur++;
+                                    self::$nombreJe++;
+                                }
+                                
+                                public function setRole(string $role):void{
+                                    $this->Role=$role;
+                                }
+
+                                public function getRole(){
+                                    return $this->Role;
+                                }
+                            }
+                            $Adm = new Administrateur('Albert_ADM');
+                            $Adm-> ajoutRole('Role NV');
+                            $Adm ->getRole();
+                            $Adm -> setStatus('STATUS NV');
+                            $Joueur = new Joueur('Albert_JEU');
+                            $Joueur -> setStatus('INACTIVE');
+                            var_dump($Adm,$Joueur,Joueur::$nombreJe, Administrateur::$nombreAdm, Utilisateur::$nombreUtilisateur);
+            ```
+            - **NB:** 
     - Supposons que l'on souhaite garantir un usage particulier, mais laisser les classes enfants decider de la maniére dont le code doit fonctionner. 
--- POO/POOTEST/index.php
+++ POO/POOTEST/index.php
@@ -1,63 +1,87 @@
 <?php
-    declare(strict_types=1);
-    class Utilisateur 
-    {
-        public const STATUS_ACTIVE = 'active';
-        public const STATUS_INACTIVE='inactive';
-        public string $Username;
-        public string $Status;
+                    declare(strict_types=1);
+                    abstract class Utilisateur
+                    {
+                        protected const STATUS_ACTIVE='ACTIVE';
+                        protected const STATUS_INACTIVE='INACTIVE';
 
-        public function __construct(string $username, string $status=self::STATUS_ACTIVE){
-            $this->Username=$username;
-            $this->Status=$status;
-        }
+                        protected string $Username;
+                        protected string $Status; 
 
-        public function setUsername(string $username):void{
-            $this->Username=$username;
-        }
+                        Public static $nombreUtilisateur = 0;
 
-        public function getUsername():string{
-            return $this->Username;
-        }
+                        public function __construct(string $username, string $status=self::STATUS_ACTIVE){
+                            $this->Username=$username;
+                            $this->Status=$status;
+                        }
+                         public function setUsername(string $username):void{
+                             $this->Username = $username;
+                         }
+                         public function getUsername():string{
+                             return $this->Username;
+                         }
 
-        Public function setStatus(string $status):void{
-            $this->Status=$status;
-        }
+                         public function setStatus(string $status):void{
+                             $this->Status=$status;
+                         }
+                         public function getStatus():string{
+                             return $this->Status;
+                         }
+                    }
+                         class Administrateur extends Utilisateur
+                            {
+                                private array $Role=[];
 
-        public function getStatus():string{
-            return $this->Status;
-        }
-    }
-    class Administrateur extends Utilisateur
-    {
-        private array $Role=[];
+                                public static $nombreAdm=0;
 
-        public function __construct(string $username, string $status=self::STATUS_ACTIVE, array $role=[]){
-            $this->Username=$username;
-            $this->Status=$status;
-            $this->Role=$role;
-        }
+                                public function __construct(string $username, string $status=self::STATUS_ACTIVE, array $role=[]){
+                                    parent::__construct($username,$status);
+                                    $this->Role=$role;
+                                    parent::$nombreUtilisateur++;
+                                    self::$nombreAdm++;
+                                }
+                                public function ajoutRole(string $role){
+                                    $this->Role[]=$role;
+                                    $this->Role = array_filter($this->Role);   
+                                }
 
-        public function ajoutRole(string $role){
-            $this->Role[]=$role;
-            $this->Role = array_filter($this->Role);
-        }
+                                public function setRole(string $role):void{
+                                    $this->Role=$role;
+                                }
 
-        Public function setRole(string $role):void{
-            $this->Role=$role;
-        }
+                                public function getRole():array{
+                                    $role=$this->Role;
+                                    $role[]='ADMINISTRATEUR';
 
-        public function getRole():array{
-            $role =$this->Role;
-            $role[]='ADMIN';
+                                    return $role;
+                                }
+                            }
+                             class Joueur extends Utilisateur
+                            {
+                                protected const Role='JOUEUR';
+                                protected string $Role;
 
-            return $role;
-        }
-    }
-$Albert_Uti = new Utilisateur('albert_Uti');
-$Albert_Uti->setStatus('inactive');
-$Albert_Adm =new Administrateur('albert_Adm');
-$Albert_Adm->ajoutRole('Administrateur');
-$Albert_Adm->setStatus('inactive');
+                                public static $nombreJe = 0;
 
-var_dump($Albert_Adm);
+                                public function __construct(string $username, string $status=self::STATUS_ACTIVE, string $role=self::Role){
+                                    parent::__construct($username,$status);
+                                    $this->Role=$role;
+                                    parent::$nombreUtilisateur++;
+                                    self::$nombreJe++;
+                                }
+                                
+                                public function setRole(string $role):void{
+                                    $this->Role=$role;
+                                }
+
+                                public function getRole(){
+                                    return $this->Role;
+                                }
+                            }
+                            $Adm = new Administrateur('Albert_ADM');
+                            $Adm-> ajoutRole('Role NV');
+                            $Adm ->getRole();
+                            $Adm -> setStatus('STATUS NV');
+                            $Joueur = new Joueur('Albert_JEU');
+                            $Joueur -> setStatus('INACTIVE');
+                            var_dump($Adm,$Joueur,Joueur::$nombreJe, Administrateur::$nombreAdm, Utilisateur::$nombreUtilisateur);
