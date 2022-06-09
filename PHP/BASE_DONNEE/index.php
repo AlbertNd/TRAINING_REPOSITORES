2558f0b3ae9111f4e81c575ae19cb4a95b547e45
Albert Ndizeye <albertndizeye@gmail.com>
2022-06-09 16:42:14 +0200

    suite

--- PHP/BASE_DONNEE/index.php
+++ PHP/BASE_DONNEE/index.php
@@ -1,7 +1,21 @@
 <?php
 
 try {
-	$db = new PDO();
+	$db = new PDO('mysql:hot=localhost;dbname=blog;charset=utf8','user','Albert10?', [PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION]);
 }catch (Exception $e){
 
+	die('Erreur : '.$e->getMessage());
 }
+
+$declarationDb = $db -> prepare("SELECT*FROM utilisateur WHERE Nom = ?");
+$declarationDb -> execute(['Ndizeye']);
+$nom = $declarationDb -> fetchAll(); 
+
+foreach($nom as $nom){
+	?>
+	<div>
+		<?php echo $nom['Nom'] .' et le prenom '. $nom['prenom']?>
+	</div>
+<?php
+}
+?>
