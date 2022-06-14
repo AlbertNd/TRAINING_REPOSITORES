46d2825db5f4a9a2bbdf543e41db98f09eb51144
Albert Ndizeye <albertndizeye@gmail.com>
2022-06-14 22:57:32 +0200

    suite

--- PHP/PROJET_RECAP/Control/ConnectionBD.php
+++ PHP/PROJET_RECAP/Control/ConnectionBD.php
@@ -1,19 +1,27 @@
 <?php
 
 
-try {
-    $bd = new PDO('mysql:host=localhost;dbname=Formation;charset=utf8', 'user', 'Albert10?', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
+function connectionDB(){
+    try {
+        $bd = new PDO('mysql:host=localhost;dbname=Formation;charset=utf8', 'user', 'Albert10?', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        return $bd;
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
+
+function lireDonne()
+{
+
+    $connectionUtilisateur = connectionDB()->prepare('SELECT * FROM Utilisateur WHERE Email = :Email');
+    $connectionUtilisateur->execute([
+        'Email' => $_POST['email']
+    ]);
+    $connectionU = $connectionUtilisateur->fetchAll();
+    return $connectionU;
 }
 
 $utilisateurList = $bd->prepare('SELECT * FROM Utilisateur');
 $utilisateurList->execute();
-$utilisateur= $utilisateurList->fetchAll();
-
-
-$connectionUtilisateur = $bd -> prepare('SELECT * FROM Utilisateur WHERE Email = :Email');
-$connectionUtilisateur -> execute([
-    'Email' => $_POST['email']
-]);
-$connectionU = $connectionUtilisateur ->fetchAll();
+$utilisateur = $utilisateurList->fetchAll();
