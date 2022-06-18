1ba02b5008c2433f21dc5760cb1451249ff07c7c
Albert Ndizeye <albertndizeye@gmail.com>
2022-06-19 00:44:01 +0200

    suite

--- PHP/PROJET_RECAP/Vues/Acceuille.php
+++ PHP/PROJET_RECAP/Vues/Acceuille.php
@@ -1,11 +1,11 @@
+<div class="container flex justify-center">
+	<?php foreach ($affichAcc->lireBD($_POST['email']) as $utilisateur) { ?>
 
-<?php foreach($affichAcc -> lireBD($_POST['email']) as $utilisateur){ ?>
-    
-	<div>
-		<?php echo $utilisateur['nom'] .' et le prenom '. $utilisateur['prenom']?>
-	</div>
+		<div>
+			<?php echo 'Le nom du gars qui est connecté c\'est bien '.$utilisateur['nom'] . ' et le prenom ' . $utilisateur['prenom'] ?>
+		</div>
 
-<?php
-}
-?>
- 
+	<?php
+	}
+	?>
+</div>
