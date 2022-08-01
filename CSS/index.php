c84ac761071704c9ef91e1a787d9e95656174822
Albert Ndizeye <albertndizeye@gmail.com>
2022-08-02 00:07:43 +0200

    suite

--- CSS/index.php
+++ CSS/index.php
@@ -10,14 +10,31 @@
 </head>
 
 <body>
-    <nav>
-            <h1>Logo</h1>
-        <div class="box-lien">
-            <a href="">Lien 1</a>
-            <a href="">Lien 2</a>
-            <a href="">Lien 3</a>
-        </div>
-    </nav>
+    <div class="content-header">
+        <nav>
+            <div class="box-log">
+                <div class="box-text">
+                    <h1> Art-Bel Transport </h1>
+                </div>
+                <div class="box-logo">
+                    <?php include('logo.php') ?>
+                </div>
+
+            </div>
+
+            <div class="box-lien">
+                <a href="">Lien 1</a>
+                <a href="">Lien 2</a>
+                <a href="">Lien 3</a>
+            </div>
+        </nav>
+        <header class="box-header">
+            <h1>Transport de marchandise pour compte de tiers</h1>
+            <h4>Solutions entrepreunariales</h4>
+            <button>Contacter nous</button>
+        </header>
+    </div>
+
 </body>
 
 </html>
