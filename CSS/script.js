4b2d0b8d655196fc9de53b0a426553f8f39093ce
Albert Ndizeye <albertndizeye@gmail.com>
2022-08-08 00:21:36 +0200

    suite

--- CSS/script.js
+++ CSS/script.js
@@ -1,8 +1,7 @@
+/* Nava bar sticky on top */
 window.onscroll = function() {myFunction()};
-
 var navbar = document.getElementById("box-nav");
 var sticky = navbar.offsetTop;
-
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
@@ -10,3 +9,5 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+/* Responsive burger button */ 
